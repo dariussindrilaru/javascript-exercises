c13d0adea83e6c7cbf69efa81debf93d1a4e5666
Darius <133234590+dariussindrilaru@users.noreply.github.com>
2023-12-18 14:48:19 +0200

    Finish fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    if (number < 0) {
+        return 'OOPS';
+    }
 
+    let current = 0;
+    let next = 1;
+
+    for (let i = 1; i <= number; i++) {
+        let sum = current + next;
+        current = next;
+        next = sum;
+    } return current;
 };
 
 // Do not edit below this line
