d011a1d1b956f33a93cedcf7df37b939582e6d9c
Darius <133234590+dariussindrilaru@users.noreply.github.com>
2023-12-18 14:47:01 +0200

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let str = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    return str == str.split('').reverse().join('');
 };
 
 // Do not edit below this line
