f6bde7ee118d20bbb4a04b6eb8a1b463681f1bbe
Darius <133234590+dariussindrilaru@users.noreply.github.com>
2023-12-18 16:37:13 +0200

    Finish getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+    const bookTitle = obj.map((book => {
+        return book.title;
+    }));
+    return bookTitle;
 };
 
 // Do not edit below this line
