6f22043965d79c0fe70c19a7c5cdec7b5332b0c0
Darius <133234590+dariussindrilaru@users.noreply.github.com>
2023-12-19 19:40:07 +0200

    Finish findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const oldest = people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
 
+        if (oldestAge > currentAge) {
+            return oldest;
+        } else {
+            return currentPerson;
+        }
+    });
+    return oldest;
+};
+
+const getAge = function (birth, death) {
+    if (!death) {
+        return new Date().getFullYear() - birth;
+    } else {
+        return death - birth;
+    }
 };
 
 // Do not edit below this line
